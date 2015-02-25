43e63029dc54078a8e80545c8c812894def429ad
Sistiandy <sistiandy@artikulpi.com>
2015-02-25 11:24:13 +0700

    Penambahan config untuk pagination

---
+++ application/config/pagination.php
@@ -0,0 +1,22 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config = array('per_page' => 10,
+                'uri_segment' => 4,
+                'full_tag_open' => '<ul class="pagination">',
+                'full_tag_close' => '</ul>',
+                'next_tag_open' => '<li>',
+                'num_tag_open' => '<li>',
+                'prev_tag_open' => '<li>',
+                'first_tag_open' => '<li>',
+                'last_tag_open' => '<li>',
+                'next_tag_close' => '</li>',
+                'num_tag_close' => '</li>',
+                'prev_tag_close' => '</li>',
+                'first_tag_close' => '</li>',
+                'last_tag_close' => '</li>',
+                'cur_tag_open' => '<li  class="active"><a href="#">',
+                'cur_tag_close' => '</a></li>'
+                );
+
+/* End of file pagination.php */
+/* Location: ./application/config/pagination.php */
