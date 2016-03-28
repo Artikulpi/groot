4d5ba66162ba7260bfaf73c3e3b61f6bf9ecd78d
Yudi Rustiandi <yudirustiandi@Yudis-MacBook.local>
2016-03-28 17:00:24 +0700

    Mengganti dengan groot v2

--- application/libraries/G_composer.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class G_composer 
-{
-    function __construct() 
-    {
-        include "./vendor/autoload.php";
-    }
-}
--- application/views/layout.php
+++
@@ -1,3 +0,0 @@
-<?php $this->template->load('header'); ?>
-<?php $this->template->load($main); ?>
-<?php $this->template->load('footer'); ?>
