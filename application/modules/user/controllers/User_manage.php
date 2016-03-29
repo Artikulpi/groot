29ec315db8e600ab3d6d109a18c3afa651f68924
Yudi Rustiandi <yudirustiandi@Yudis-MacBook.local>
2016-03-29 16:55:48 +0700

    update groot v2

---
+++ application/modules/user/controllers/User_manage.php
@@ -0,0 +1,169 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User_manage extends CI_Controller {
+
+	public function __construct()
+	{
+        parent::__construct();
+        $this->load->model('User_model');
+        //$this->load->model('Activity_log_model');
+        $this->load->helper(array('form', 'url'));
+
+        if ($this->session->userdata('logged_admin') == NULL)
+            redirect('user/auth/login');
+    }
+
+    // User_customer view in list
+    public function index($offset = NULL)
+    {
+        $this->load->library('pagination');
+
+        $data['user'] = $this->User_model->get(array('limit' => 10, 'offset' => $offset));
+        $data['title'] = 'Pengguna';
+        $data['main'] = 'user/user_list';
+        $config['base_url'] = site_url('user/index');
+        $config['total_rows'] = count($this->User_model->get());
+        $this->pagination->initialize($config);
+
+        $this->load->view('manage/layout', $data);
+    }
+
+    // Add User_customer and Update
+    public function add($id = NULL) {
+        $this->load->library('form_validation');
+
+        if (!$this->input->post('user_id')) {
+            $this->form_validation->set_rules('user_password', 'password', 'required|matches[passconf]|min_length[6]|');
+            $this->form_validation->set_rules('passconf', 'Password Confirmation', 'required|min_length[6]|max_length[20]|');
+            $this->form_validation->set_rules('user_name', 'Username', 'required|is_unique[g_user.user_name]');
+        }
+
+        $this->form_validation->set_rules('user_full_name', 'Name', 'required');
+        $this->form_validation->set_rules('user_email', 'User Email', 'required|valid_email');
+        $this->form_validation->set_error_delimiters('<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>', '</div>');
+        $data['operation'] = isset($id) ? 'Edit' : 'Tambah';
+
+        if ($_POST AND $this->form_validation->run() == TRUE) {
+
+            if ($this->input->post('user_id')) {
+                $params['user_id'] = $this->input->post('user_id');
+            } else {
+                $params['user_name'] = $this->input->post('user_name');
+                $params['user_input_date'] = date('Y-m-d H:i:s');
+                $params['user_password'] = sha1($this->input->post('user_password'));
+            }
+            $params['user_role'] = $this->input->post('role_id');
+            $params['user_last_update'] = date('Y-m-d H:i:s');
+            $params['user_full_name'] = $this->input->post('user_full_name');
+            $params['user_description'] = $this->input->post('user_description');
+            $params['user_email'] = $this->input->post('user_email');
+            $status = $this->User_model->add($params);
+
+            // activity log
+            $this->Activity_log_model->add(
+                    array(
+                        'activity_log_date' => date('Y-m-d H:i:s'),
+                        'user_id' => $this->session->userdata('user_id_admin'),
+                        'activity_log_module' => 'Pengguna',
+                        'activity_log_action' => $data['operation'],
+                        'activity_log_info' => 'ID:null;Title:' . $this->input->post('user_name')
+                    )
+            );
+
+            $this->session->set_flashdata('success', $data['operation'] . ' Pengguna berhasil');
+            redirect('manage/user');
+        } else {
+            if ($this->input->post('user_id')) {
+                redirect('manage/user/edit/' . $this->input->post('user_id'));
+            }
+
+            // Edit mode
+            if (!is_null($id)) {
+                if ($this->User_model->get(array('id' => $id)) == NULL) {
+                    redirect('manage/user');
+                } else {
+                    $data['user'] = $this->User_model->get(array('id' => $id));
+                }
+            }
+            $data['role'] = $this->User_model->get_role();
+            $data['button'] = ($id == $this->session->userdata('user_id_admin')) ? 'Ubah' : 'Reset';
+            $data['title'] = $data['operation'] . ' Pengguna';
+            $data['main'] = 'user/user_add';
+            $this->load->view('admin/layout', $data);
+        }
+    }
+
+    function detail($id = NULL) {
+        if ($this->User_model->get(array('id' => $id)) == NULL) {
+            redirect('manage/user');
+        }
+        $data['user'] = $this->User_model->get(array('id' => $id));
+        $data['title'] = 'Detail pengguna';
+        $data['main'] = 'user/user_detail';
+        $this->load->view('admin/layout', $data);
+    }
+
+    function rpw($id = NULL) {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('user_password', 'Password', 'required|matches[passconf]|min_length[6]|');
+        $this->form_validation->set_rules('passconf', 'Password Confirmation', 'required|min_length[6]|');
+        $this->form_validation->set_error_delimiters('<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>', '</div>');
+        if ($_POST AND $this->form_validation->run() == TRUE) {
+            $id = $this->input->post('user_id');
+            $params['user_password'] = sha1($this->input->post('user_password'));
+            $status = $this->User_model->change_password($id, $params);
+
+            // activity log
+            $this->Activity_log_model->add(
+                    array(
+                        'activity_log_date' => date('Y-m-d H:i:s'),
+                        'user_id' => $this->session->userdata('user_id_admin'),
+                        'activity_log_module' => 'Pengguna',
+                        'activity_log_action' => 'Reset Password',
+                        'activity_log_info' => 'ID:null;Title:' . $this->input->post('user_name')
+                    )
+            );
+            $this->session->set_flashdata('success', 'Reset password pengguna berhasil');
+            redirect('manage/user');
+        } else {
+            if ($this->User_model->get(array('id' => $id)) == NULL) {
+                redirect('manage/user');
+            }
+            $data['user'] = $this->User_model->get(array('id' => $id));
+            $data['title'] = 'Ganti Password Pengguna';
+            $data['main'] = 'user/change_pass';
+            $this->load->view('admin/layout', $data);
+        }
+    }
+
+    // Delete User
+    public function delete($id = NULL) {
+        if ($this->User_model->get(array('id' => $id)) == NULL) {
+            redirect('manage/user');
+        }
+        if ($_POST) {
+
+            $this->User_model->delete($this->input->post('del_id'));
+            // activity log
+            $this->Activity_log_model->add(
+                    array(
+                        'activity_log_date' => date('Y-m-d H:i:s'),
+                        'user_id' => $this->session->userdata('user_id_admin'),
+                        'activity_log_module' => 'Pengguna',
+                        'activity_log_action' => 'Hapus',
+                        'activity_log_info' => 'ID:' . $this->input->post('del_id') . ';Title:' . $this->input->post('del_name')
+                    )
+            );
+            $this->session->set_flashdata('success', 'Hapus pengguna berhasil');
+            redirect('manage/user');
+        } elseif (!$_POST) {
+            $this->session->set_flashdata('delete', 'Delete');
+            redirect('manage/user/edit/' . $id);
+        }
+    }
+
+}
+
+/* End of file User_manage.php */
+/* Location: ./application/modules/user/controllers/User_manage.php */
---
+++ application/modules/user/models/User_model.php
@@ -0,0 +1,218 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User_model extends CI_Model {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	var $table = 'user';
+
+	// Get From Databases
+    function get($params = array())
+    {
+        $this->db->select('user.user_id, user_name, user_password, user_full_name, user_description,
+            user_email, user_input_date, user_last_update, user.user_role, user_role.role_name'); 
+        
+        if(isset($params['id']))
+        {
+            $this->db->where('user.user_id', $params['id']);
+        }
+        elseif(isset($params['user_id']))
+        {
+            $this->db->where('user.user_id', $params['user_id']);
+        }
+        elseif(isset($params['name']))
+        {
+            $this->db->like('user_name', $params['name']);
+        }
+        elseif(isset($params['date']))
+        {
+            $this->db->where('user_input_date', $params['date']);
+        }
+        
+        if(isset($params['user_role']))
+        {
+            $this->db->where('user_role', $params['user_role']);
+        }
+
+        if(isset($params['limit']))
+        {
+            if(!isset($params['offset']))
+            {
+                $params['offset'] = NULL;
+            }
+
+            $this->db->limit($params['limit'], $params['offset']);
+        }
+
+        if(isset($params['order_by']))
+        {
+            $this->db->order_by($params['order_by'], 'desc');
+        }
+        else
+        {
+            $this->db->order_by('user_last_update', 'desc');
+        }
+
+        $this->db->join('user_role', 'user_role.role_id = user.user_role_id', 'left');
+        $res = $this->db->get('user');
+
+        if(isset($params['id']))
+        {
+            return $res->row_array();
+        }
+        else
+        {
+            return $res->result_array();
+        }
+    }
+    
+    // Get Role From Databases
+    function get_role($params = array())
+    {
+        $this->db->select('user_role.role_id, role_name, role_input_date, role_last_update'); 
+        
+        if(isset($params['id']))
+        {
+            $this->db->where('user_role.role_id', $params['id']);
+        }
+        if(isset($params['role_id']))
+        {
+            $this->db->where('user_role.role_id', $params['role_id']);
+        }
+        
+        if(isset($params['limit']))
+        {
+            if(!isset($params['offset']))
+            {
+                $params['offset'] = NULL;
+            }
+
+            $this->db->limit($params['limit'], $params['offset']);
+        }
+
+        if(isset($params['order_by']))
+        {
+            $this->db->order_by($params['order_by'], 'desc');
+        }
+        else
+        {
+            $this->db->order_by('role_last_update', 'desc');
+        }
+        
+        $res = $this->db->get('user_role');
+
+        if(isset($params['id']))
+        {
+            return $res->row_array();
+        }
+        else
+        {
+            return $res->result_array();
+        }
+    }
+
+    function add($data = array()) {
+        
+         if(isset($data['user_id'])) {
+            $this->db->set('user_id', $data['user_id']);
+        }
+        
+         if(isset($data['user_name'])) {
+            $this->db->set('user_name', $data['user_name']);
+        }
+        
+         if(isset($data['user_password'])) {
+            $this->db->set('user_password', $data['user_password']);
+        }
+        
+         if(isset($data['user_full_name'])) {
+            $this->db->set('user_full_name', $data['user_full_name']);
+        }
+        
+         if(isset($data['user_email'])) {
+            $this->db->set('user_email', $data['user_email']);
+        }
+        
+         if(isset($data['user_description'])) {
+            $this->db->set('user_description', $data['user_description']);
+        }
+        
+         if(isset($data['user_input_date'])) {
+            $this->db->set('user_input_date', $data['user_input_date']);
+        }
+        
+         if(isset($data['user_last_update'])) {
+            $this->db->set('user_last_update', $data['user_last_update']);
+        }
+        
+         if(isset($data['user_role'])) {
+            $this->db->set('user_role', $data['user_role']);
+        }
+        
+        if (isset($data['user_id'])) {
+            $this->db->where('user_id', $data['user_id']);
+            $this->db->update('user');
+            $id = $data['user_id'];
+        } else {
+            $this->db->insert('user');
+            $id = $this->db->insert_id();
+        }
+
+        $status = $this->db->affected_rows();
+        return ($status == 0) ? FALSE : $id;
+    }
+
+    function add_role($data = array()) {
+        
+         if(isset($data['role_id'])) {
+            $this->db->set('role_id', $data['role_id']);
+        }
+        
+         if(isset($data['role_name'])) {
+            $this->db->set('role_name', $data['role_name']);
+        }
+        
+         if(isset($data['role_input_date'])) {
+            $this->db->set('role_input_date', $data['role_input_date']);
+        }
+        
+         if(isset($data['role_last_update'])) {
+            $this->db->set('role_last_update', $data['role_last_update']);
+        }
+        
+        if (isset($data['role_id'])) {
+            $this->db->where('role_id', $data['role_id']);
+            $this->db->update('user_role');
+            $id = $data['role_id'];
+        } else {
+            $this->db->insert('user_role');
+            $id = $this->db->insert_id();
+        }
+
+        $status = $this->db->affected_rows();
+        return ($status == 0) ? FALSE : $id;
+    }
+
+    function delete($id) {
+        $this->db->where('user_id', $id);
+        $this->db->delete('user');
+    }
+
+    function delete_role($id) {
+        $this->db->where('role_id', $id);
+        $this->db->delete('user_role');
+    }
+
+    function change_password($id, $params){
+       $this->db->where('user_id', $id);
+       $this->db->update('user', $params);   
+   }
+
+}
+
+/* End of file User_model.php */
+/* Location: ./application/modules/user/models/User_model.php */
