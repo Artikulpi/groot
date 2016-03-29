27127840c704cf5a1318066787a213e4a79fcd99
Yudi Rustiandi <yudirustiandi@Yudis-MacBook.local>
2016-03-29 17:00:34 +0700

    Update groot v2

--- db/data.sql
+++ db/data.sql
@@ -1,3 +1,25 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 29, 2016 at 11:48 AM
+-- Server version: 5.5.42
+-- PHP Version: 5.6.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `groot`
+--
+
+--
+-- Dumping data for table `posting_category`
+--
+
+INSERT INTO `posting_category` (`category_id`, `category_name`, `category_input_date`, `category_last_update`) VALUES
+(1, 'Bisnis', '2016-03-26 08:09:41', '2016-03-26 08:09:41');
 
 --
 -- Dumping data for table `user`
@@ -13,15 +35,3 @@ INSERT INTO `user` (`user_id`, `user_name`, `user_full_name`, `user_password`, `
 INSERT INTO `user_role` (`role_id`, `role_name`, `role_input_date`, `role_last_update`) VALUES
 (1, 'Admin', '2016-03-23 07:44:17', '2016-03-23 07:44:17'),
 (2, 'Super Admin', '2016-03-23 07:44:17', '2016-03-23 07:44:17');
-
---
--- Dumping data for table `posting_category`
---
-
-INSERT INTO `posting_category` (`category_id`, `category_name`, `category_input_date`, `category_last_update`) VALUES
-(1, 'Bisnis', '2016-03-26 08:09:41', '2016-03-26 08:09:41'),
-(3, 'Teknologi', '2016-03-26 08:28:34', '2016-03-26 08:37:31'),
-(4, 'Gadget', '2016-03-26 08:38:54', '2016-03-26 08:38:54'),
-(6, 'Programming', '2016-03-26 08:49:19', '2016-03-26 08:49:41'),
-(7, 'Desain Web', '2016-03-26 09:14:10', '2016-03-26 09:14:32'),
-(8, 'Linux', '2016-03-27 21:54:36', '2016-03-27 21:54:36');
