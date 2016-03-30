452433dfcf47061d2eac0f856ed3bd3b180d327d
Muhammad Nazih <nazih@artikulpi.com>
2016-03-30 15:08:08 +0700

    Design:Modifikasi template groot

--- application/views/manage/sidebar.php
+++ application/views/manage/sidebar.php
@@ -1,126 +1,121 @@
-<div class="col-sm-3 col-md-2 sidebar-offcanvas padd-null" id="sidebar" role="navigation">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+            <div class="nav-side-menu">
+                <div class="menu-list">
+                    <div class="sidebar-block">
+                        <div class="media">
+                            <div class="media-left">
+                                <a href="page-profile.html">
+                                    <span class="status dotted dotted-primary" data-toggle="tooltip" data-container="body" title="" data-original-title="available"></span>
+                                    <img class="media-object img-circle" src="<?php echo media_url('image/profil.png') ?>" alt="photo profile">
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                <h4 class="media-heading"><?php echo $text = ucfirst($this->session->userdata('uname')); ?></h4>
+                                <p class="text-muted">
+                                    <small><?php echo $this->session->userdata('uemail'); ?></small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
 
-    <div class="list">
-        <div class="panel panel-default pipanel">
-            <div class="panel-heading">
-                <a data-toggle="collapse" href="#navdashboard">
-                    <h4 class="title">Dashboard</h4>
-                </a>
+                    <ul id="menu-content" class="menu-content collapse out">
+                        <li class="active">
+                            <a href="<?php echo site_url('manage/dashboard'); ?>">
+                                <i class="ion-speedometer"></i> Dashboard
+                            </a>
+                        </li>
+
+                        <li  data-toggle="collapse" data-target="#posting" class="collapsed">
+                            <a href="#"><i class="ion-folder"></i> Posting <span class="arrow"></span></a>
+                        </li>
+
+                        <ul class="sub-menu collapse" id="posting">
+                            <li class="active"><a href="<?php echo site_url('manage/posting'); ?>">
+                               Daftar Posting</a></li>
+                               <li><a href="<?php echo site_url('manage/posting/add'); ?>"> 
+                                 Tambah Posting</a></li>
+                                 <li><a href="<?php echo site_url('manage/posting/category'); ?>">
+                                     Daftar Kategori</a></li>
+                                     <li><a href="<?php echo site_url('manage/posting/add_category'); ?>"> 
+                                         Tambah Kategori</a></li>
+                                     </ul>
+
+                        <li  data-toggle="collapse" data-target="#halaman" class="collapsed">
+                            <a href="#"><i class="ion-folder"></i> Halaman <span class="arrow"></span></a>
+                        </li>
+                        <ul class="sub-menu collapse <?php echo ($this->uri->segment(2) == 'page') ? 'in' : NULL; ?>" id="halaman">
+                            <li class="active"><a href="<?php echo site_url('manage/page'); ?>">
+                             Daftar Halaman</a></li>
+                             <li><a href="<?php echo site_url('manage/page/add'); ?>">
+                                 Tambah Halaman</a></li>
+                                 <li><a href="<?php echo site_url('manage/page/tree'); ?>">
+                                     Susunan Menu</a></li> 
+                                 </ul>
+
+                        <li  data-toggle="collapse" data-target="#contact" class="collapsed">
+                            <a href="#"><i class="ion-folder"></i> Contact <span class="arrow"></span></a>
+                        </li>
+                        <ul class="sub-menu collapse <?php echo ($this->uri->segment(2) == 'contact') ? 'in' : NULL; ?>" id="contact">
+                            <li class="active"><a href="<?php echo site_url('manage/contact'); ?>">
+                             Contact</a></li>
+                                 </ul>
+
+                        <li  data-toggle="collapse" data-target="#pengguna" class="collapsed">
+                            <a href="#"><i class="ion-folder"></i> Pengguna <span class="arrow"></span></a>
+                        </li>
+                        <ul class="sub-menu collapse <?php echo ($this->uri->segment(2) == 'user') ? 'in' : NULL; ?>" id="pengguna">
+                            <li class="active"><a href="<?php echo site_url('manage/user'); ?>">
+                                 Daftar penguna</a></li>
+                                  <li><a href="<?php echo site_url('manage/user/add'); ?>"> 
+                                 Tambah pengguna</a></li>
+                                 </ul>
+
+                        <li  data-toggle="collapse" data-target="#media" class="collapsed">
+                            <a href="#"><i class="ion-folder"></i> Media Manager <span class="arrow"></span></a>
+                        </li>
+                        <ul class="sub-menu collapse <?php echo ($this->uri->segment(2) == 'media_manager' OR $this->uri->segment(2) == 'media_album') ? 'in' : NULL; ?>" id="media">
+                            <li class="active"><a href="<?php echo site_url('manage/media_manager'); ?>">
+                                Image List</a></li>
+                                  <li><a href="<?php echo site_url('manage/media_album'); ?>"> 
+                                 Album List</a></li>
+                                 </ul>
+
+                        <li  data-toggle="collapse" data-target="#setting" class="collapsed">
+                            <a href="#"><i class="ion-folder"></i> Pengaturan <span class="arrow"></span></a>
+                        </li>
+                        <ul class="sub-menu collapse <?php echo ($this->uri->segment(2) == 'setting') ? 'in' : NULL; ?>" id="setting">
+                            <li class="active"><a href="<?php echo site_url('manage/setting'); ?>">
+                                     General</a></li>
+                                  <li><a href="<?php echo site_url('manage/setting/email'); ?>">
+                                     Email</a></li>
+                                 </ul>
+
+                        <li  data-toggle="collapse" data-target="#log" class="collapsed">
+                            <a href="#"><i class="ion-folder"></i> Log Aktivitas <span class="arrow"></span></a>
+                        </li>
+                        <ul class="sub-menu collapse <?php echo ($this->uri->segment(2) == 'activity_log') ? 'in' : NULL; ?>" id="log">
+                            <li class="active"><a href="<?php echo site_url('manage/activity_log'); ?>">
+                                 Daftar Log Aktivitas</a></li>
+                                 </ul>
+                
+                    </ul>
+                    <div class="sidebar-block">
+                        <div class="media">
+                            <div class="media-left">
+                            </div>
+                            <div class="media-body">
+                                <h4 class="media-heading">Notification</h4>
+                                <p class="text-muted">
+                                    <small><?php
+                                    if ($this->session->flashdata('success')) {
+                                        $data['message'] = $this->session->flashdata('success');
+                                        $this->load->view('admin/notification_success', $data);
+                                    }
+                                    ?> </small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <br><br><br>
+                </div>
             </div>
-
-            <ul id="navdashboard" class="list-group panel-collapse collapse">
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/dashboard'); ?>">
-                    <span class="glyphicon glyphicon-home"></span> Dashboard</a>
-            </ul>
-        </div><!-- Dashboard -->
-
-        <div class="panel panel-default pipanel">
-            <div class="panel-heading">
-                <a data-toggle="collapse" href="#navpost">
-                    <h4 class="title">Posting</h4>
-                </a>
-            </div>
-
-            <ul id="navpost" class="list-group panel-collapse collapse">
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/posting'); ?>">
-                    <span class="glyphicon glyphicon-list"></span> Daftar Posting</a>
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/posting/add'); ?>"> 
-                    <span class="glyphicon glyphicon-plus-sign"></span> Tambah Posting</a>
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/posting/category'); ?>">
-                    <span class="glyphicon glyphicon-list"></span> Daftar Kategori</a>
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/posting/add_category'); ?>"> 
-                    <span class="glyphicon glyphicon-plus-sign"></span> Tambah Kategori</a>
-            </ul>
-        </div><!-- posting -->
-
-        <div class="panel panel-default pipanel">
-            <div class="panel-heading">
-                <a data-toggle="collapse" href="#navpage">
-                    <h4 class="title">Halaman</h4>
-                </a>
-            </div>
-
-            <ul id="navpage" class="list-group panel-collapse collapse <?php echo ($this->uri->segment(2) == 'page') ? 'in' : NULL; ?>">
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/page'); ?>">
-                    <span class="glyphicon glyphicon-list"></span> Daftar Halaman</a>
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/page/add'); ?>">
-                    <span class="glyphicon glyphicon-plus-sign"></span> Tambah Halaman</a>
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/page/tree'); ?>">
-                    <span class="glyphicon glyphicon-align-left"></span> Susunan Menu</a>
-            </ul>
-        </div><!-- Page -->
-
-        <div class="panel panel-default pipanel">
-            <div class="panel-heading">
-                <a data-toggle="collapse" href="#navcont">
-                    <h4 class="title">Contact</h4>
-                </a>
-            </div>
-
-            <ul id="navcont" class="list-group panel-collapse collapse <?php echo ($this->uri->segment(2) == 'contact') ? 'in' : NULL; ?>">
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/contact'); ?>">
-                    <span class="glyphicon glyphicon-list"></span> Contact</a>
-            </ul>
-        </div><!-- User -->
-
-        <div class="panel panel-default pipanel">
-            <div class="panel-heading">
-                <a data-toggle="collapse" href="#navuser">
-                    <h4 class="title">Pengguna</h4>
-                </a>
-            </div>
-
-            <ul id="navuser" class="list-group panel-collapse collapse">
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/user'); ?>">
-                    <span class="glyphicon glyphicon-list"></span> Daftar penguna</a>
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/user/add'); ?>"> 
-                    <span class="glyphicon glyphicon-plus-sign"></span> Tambah pengguna</a>
-            </ul>
-        </div><!-- User -->
-
-        <div class="panel panel-default pipanel">
-            <div class="panel-heading">
-                <a data-toggle="collapse" href="#navmm">
-                    <h4 class="title">Media Manager</h4>
-                </a>
-            </div>
-
-            <ul id="navmm" class="list-group panel-collapse collapse <?php echo ($this->uri->segment(2) == 'media_manager' OR $this->uri->segment(2) == 'media_album') ? 'in' : NULL; ?>">
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/media_manager'); ?>">
-                    <span class="ion-images"></span> Image List</a>
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/media_album'); ?>"> 
-                    <span class="ion-folder"></span> Album List</a>
-            </ul>
-        </div><!-- Media Manager -->
-
-        <div class="panel panel-default pipanel">
-            <div class="panel-heading">
-                <a data-toggle="collapse" href="#navset">
-                    <h4 class="title">Pengaturan</h4>
-                </a>
-            </div>
-
-            <ul id="navset" class="list-group panel-collapse collapse <?php echo ($this->uri->segment(2) == 'setting') ? 'in' : NULL; ?>">
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/setting'); ?>">
-                    <span class="glyphicon glyphicon-cog"></span> General</a>
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/setting/email'); ?>">
-                    <span class="glyphicon glyphicon-cog"></span> Email</a>
-            </ul>
-        </div><!-- Media Manager -->
-
-        <div class="panel panel-default pipanel">
-            <div class="panel-heading">
-                <a data-toggle="collapse" href="#navlog">
-                    <h4 class="title">Log Aktivitas</h4>
-                </a>
-            </div>
-
-            <ul id="navlog" class="list-group panel-collapse collapse <?php echo ($this->uri->segment(2) == 'activity_log') ? 'in' : NULL; ?>">
-                <a class="lgitem list-group-item" href="<?php echo site_url('manage/activity_log'); ?>">
-                    <span class="glyphicon glyphicon-dashboard"></span> Daftar Log Aktivitas</a>
-            </ul>
-        </div><!-- Log Aktifitas -->
-
-    </div><!-- List -->
-
-</div>
