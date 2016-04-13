888d65a09c5d1ee1a80203fe5236ad580d8f1955
Muhammad Nazih <nazih@artikulpi.com>
2016-04-13 10:46:42 +0700

    Design:Memperbaiki tampilan list halaman

--- application/modules/page/views/page_list.php
+++ application/modules/page/views/page_list.php
@@ -1,48 +1,68 @@
 
 
-    <h3 class="page-header">
-        Daftar Halaman
+<h3 class="page-header">
+    Daftar Halaman
     <a href="<?php echo site_url('manage/page/add'); ?>" ><button type="button" class="btn btn-success pull-right"><span class="glyphicon glyphicon-plus-sign"></span> Tambah</button></a>
 </h3>
 
+<div class="panel">
 
-    <div class="table-responsive">
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th class="controls" align="center">NAMA HALAMAN</th>
-                <th class="controls" align="center">PENULIS</th>
-                <th class="controls" align="center">STATUS</th>
-            </tr>
-            </thead>
+
+    <div class="panel-body">
+        <ul class="list-group list-group-dividered list-group-full">
             <?php
             if (!empty($page)) {
                 foreach ($page as $row) {
                     ?>
-            <tbody>
-                    <tr>
-                        <td ><b><a style="float: left;" href="<?php echo site_url('manage/page/edit/' . $row['page_id']); ?>" ><?php echo $row['page_name']; ?></a></b></td>
-                        <td ><?php echo $row['user_name']; ?></td>
-                        <td ><?php echo ($row['page_is_published'] == 0) ? 'Draft' : 'Terbit';?></td>
-                    </tr>
-            </tbody>
-                    <?php
-                }
-            } else {
+                    <div class="row">
+                        <li class="list-group-item">
+                            <div class="media">
+                                <div class="media-left">
+                                    <a class="avatar1 avatar-online" href="javascript:void(0)">
+                                    <img src="<?php echo media_url('image/profile.png') ?>" alt="...">
+                                        <i></i>
+                                    </a>
+                                </div>
+                                <div class="media-body">
+                                    <small class="text-muted pull-right">
+                                        <p class="data-row pull-right">
+                                            <span class="data-name"><?php echo ($row['page_is_published'] == 0) ? 'Draft' : 'Terbit';?></span>
+                                        </p>
+                                    </small>
+                                    <h4 class="media-heading"># <a style="float: left;" href="<?php echo site_url('manage/page/edit/' . $row['page_id']); ?>" ><?php echo $row['page_name']; ?></a>
+                                       / <small><i><?php echo $row['user_name']; ?></i></small></h4>
+                                   </div>
+                               </div>
+                           </li>
+                       </div>
+                       <?php   }
+                   }
+                   else { ?>
+                   <li class="list-group-item">
+                    <div class="media">
+                        <div class="media-left">
+                            <a class="avatar1 avatar-online" href="javascript:void(0)">
+                                <img src="<?php echo media_url('image/profile.png') ?>" alt="...">
+                                <i></i>
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">@ Data Kosong</h4>
+                        </div>
+                    </div>
+                </li>
+                <?php   }
                 ?>
-            <tbody>
-                <tr id="row">
-                    <td colspan="3" align="center">Data Kosong</td>
-                </tr>
-            </tbody>
-                <?php
-                }
-                ?>   
-            </table>
-        </div>
-        <div >
-            <?php echo $this->pagination->create_links(); ?>
-    </div>
 
+
+            </ul>
+            <span class="text-info">
+                <?php echo $this->pagination->create_links(); ?>
+            </span>
+        </div>
+
+
+
+    </div>
 
 
