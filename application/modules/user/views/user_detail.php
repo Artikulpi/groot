759b5977604a98bff3c4eeb840e712241d4b4d6d
Muhammad Nazih <nazih@artikulpi.com>
2016-04-18 12:18:47 +0700

    Design:Modifikasi tampilan list pengguna

--- application/modules/user/views/user_detail.php
+++ application/modules/user/views/user_detail.php
@@ -1,39 +1,39 @@
-            <h2 class="page-header">
+            <h3 class="page-header">
                 <?php echo $title ?>
                 <a href="<?php echo site_url('manage/user/edit/'.$user['user_id']); ?>" ><button type="button" class="btn btn-warning pull-right"><span class="glyphicon glyphicon-edit"></span> Edit</button></a>
-                <a href="<?php echo site_url('manage/user'); ?>" ><button type="button" class="btn btn-info pull-right"><span class="glyphicon glyphicon-arrow-left"></span> Kembali</button></a>&nbsp;&nbsp;
-            </h2>
+                <a href="<?php echo site_url('manage/user'); ?>" ><button type="button" class="btn btn-danger pull-right"><span class="glyphicon glyphicon-arrow-left"></span> Kembali</button></a>&nbsp;&nbsp;
+            </h3>
 
-    <div class="row">
-        <div class="col-md-12">
-            <table class="table table-striped">
-                <tbody>
-                    <tr>
-                        <td>Nama Singkat</td>
-                        <td>:</td>
-                        <td><?php echo $user['user_name'] ?></td>
-                    </tr>
-                    <tr>
-                        <td>Nama Lengkap</td>
-                        <td>:</td>
-                        <td><?php echo $user['user_full_name'] ?></td>
-                    </tr>
-                    <tr>
-                        <td>Email</td>
-                        <td>:</td>
-                        <td><?php echo $user['user_email'] ?></td>
-                    </tr>
-                    <tr>
-                        <td>Tanggal daftar</td>
-                        <td>:</td>
-                        <td><?php echo pretty_date($user['user_input_date'], 'l, d-m-Y', FALSE) ?></td>
-                    </tr>
-                    <tr>
-                        <td>Peran</td>
-                        <td>:</td>
-                        <td><?php echo $user['role_name']; ?></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table table-striped">
+                        <tbody>
+                            <tr>
+                                <td>Nama Singkat</td>
+                                <td>:</td>
+                                <td><?php echo $user['user_name'] ?></td>
+                            </tr>
+                            <tr>
+                                <td>Nama Lengkap</td>
+                                <td>:</td>
+                                <td><?php echo $user['user_full_name'] ?></td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td>:</td>
+                                <td><?php echo $user['user_email'] ?></td>
+                            </tr>
+                            <tr>
+                                <td>Tanggal daftar</td>
+                                <td>:</td>
+                                <td><?php echo pretty_date($user['user_input_date'], 'l, d-m-Y', FALSE) ?></td>
+                            </tr>
+                            <tr>
+                                <td>Peran</td>
+                                <td>:</td>
+                                <td><?php echo $user['role_name']; ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
--- application/modules/user/views/user_list.php
+++ application/modules/user/views/user_list.php
@@ -1,36 +1,55 @@
-    <h2 class="page-header">
+    <h3 class="page-header">
         <?php echo $title ?>
         <a href="<?php echo site_url('manage/user/add'); ?>" ><button type="button" class="btn btn-success pull-right"><span class="glyphicon glyphicon-plus-sign"></span> Tambah</button></a>
-    </h2>
+    </h3>
 
-    <table class="table table-striped">
-    	<thead>
-    		<tr>
-    			<th>Username</th>
-    			<th>Nama Lengkap</th>
-    			<th>Email</th>
-    			<th>Aksi</th>
-    		</tr>
-    	</thead>
-    	<?php if (!empty($user)) {
-    	foreach ($user as $row) { ?>
-    	<tbody>
-    		<tr>
-    			<td><?php echo $row['user_name'] ?></td>
-    			<td><?php echo $row['user_full_name'] ?></td>
-    			<td><?php echo $row['user_email'] ?></td>
-    			<td>
-    				<a href="<?php echo site_url('manage/user/detail/'.$row['user_id']) ?>" class="btn btn-warning btn-xs"><i class="glyphicon glyphicon-eye-open"></i></a>
-    				<a href="<?php echo site_url('manage/user/edit/'.$row['user_id']) ?>" class="btn btn-info btn-xs"><i class="glyphicon glyphicon-edit"></i></a>
-    			</td>
-    		</tr>
-    	</tbody>
-		<?php } }
-		else { ?>
-		<tbody>
-    		<tr>
-    			<td colspan="3">Data Kosong</td>
-    		</tr>
-    	</tbody>
-    	<?php } ?>
-    </table>
+    <?php if (!empty($user)) {
+        foreach ($user as $row) { ?>
+        <div class="member-entry"> 
+            <a href="#" class="member-img"> 
+                <img src="<?php echo media_url('image/profile.png') ?>" class="img-rounded"> 
+                <i class="fa fa-forward"></i> 
+            </a> 
+            <div class="member-details"> 
+                <h4> <a href="#"># <?php echo $row['user_name'] ?> </a> </h4> 
+                <div class="row info-list"> 
+                    <div class="col-sm-3"> 
+                        <i class="ion-android-contact"></i>
+                        Nama Lengkap <a href="#"><?php echo $row['user_full_name'] ?></a> 
+                    </div> 
+                    <div class="col-sm-3"> 
+                        <i class="ion-android-mail"></i> 
+                        Email <a href="#"><i><?php echo $row['user_email'] ?></a> 
+                    </div> 
+                    <div class="col-sm-6"> 
+                    <div class="pull-right">
+                          <a href="<?php echo site_url('manage/user/detail/'.$row['user_id']) ?>" class="btn btn-circle btn-primary btn-twitter btn-sm"><i class="glyphicon glyphicon-eye-open"></i></a>
+                           <a href="<?php echo site_url('manage/user/edit/'.$row['user_id']) ?>" class="btn btn-circle btn-warning btn-twitter btn-sm"><i class="glyphicon glyphicon-edit"></i></a>
+                    </div>
+                    </div> 
+                </div> 
+            </div> 
+        </div>
+
+       <?php   }
+   }
+   else { ?>
+
+   <div class="card-3">
+    <div class="media-body">
+        <h4 class="media-heading">@ Data Kosong</h4>
+    </div>
+</div>
+
+<?php   }
+?>
+<br>
+
+
+
+
+
+
+
+
+
