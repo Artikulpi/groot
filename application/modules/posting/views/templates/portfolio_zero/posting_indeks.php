28d3ce64edee88a410b7df1915a58e08d7f6a7f4
Muhammad Nazih <nazih@artikulpi.com>
2016-05-17 14:43:41 +0700

    Design:Modifikasi tampilan detail posting

--- application/modules/posting/views/templates/portfolio_zero/posting_indeks.php
+++ application/modules/posting/views/templates/portfolio_zero/posting_indeks.php
@@ -1,33 +1,56 @@
+<br><br><br><br class="hidden-xs">
 <div class="container ukms">
     <div class="row">
-        <div class="col-md-12">
-            <div class="produk">
-                <h1 class="menu">Daftar Berita</h1><hr>
-            </div>
-        </div>
+        <div class="row">
+      <div class="col-sm-12 more-features section-description wow fadeIn animated" style="visibility: visible; animation-name: fadeIn;">
+        <center><h2>Daftar Berita</h2></center>
+        <hr style="width:80px; background:#ddd;">
+      </div>
+    </div>
 
-        <div class="col-xs-12 col-sm-10 col-sm-offset-1">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2">
 
-            <ul class="event-list">
                 <?php foreach ($posting as $row): ?>
-
-                    <li>
-                        <time datetime="2014-07-20">
+                      <div class="directory-info-row">
+              <div class="row">
+                <div class="col-md-12 col-sm-12">
+                  <div class="panel">
+                    <div class="panel-body">
+                      <div class="media">
+                      <time datetime="2014-07-20" class="pull-right">
                             <span class="days"><?php echo pretty_date($row['posting_published_date'], 'd', FALSE) ?></span>
                             <span class="months"><?php echo pretty_date($row['posting_published_date'], 'M', FALSE) ?></span>
                         </time>
-                        <?php if ($row['posting_image'] != NULL) { ?>
-                            <img src="<?php echo $row['posting_image'] ?>" />
+                      <?php if ($row['posting_image'] != NULL) { ?>
+                           <a class="pull-left hidden-xs" href="#">
+                          <img class="thumb media-object" src="<?php echo $row['posting_image'] ?>" alt="">
+                        </a>
+                         <a class="hidden-sm hidden-md hidden-lg" href="#">
+                          <img class="thumb media-object" src="<?php echo $row['posting_image'] ?>" alt=""><p></p>
+                        </a>
                         <?php } ?>
-                        <div class="info">
-                            <h3 class="title"><a href="<?php echo posting_url($row) ?>" > <?php echo character_limiter($row['posting_title'], 40) ?></a></h3>
-                            <p><span class="ion-pricetags"></span> Kategori: <a href="<?php echo site_url('news/category/' . $row['posting_category_id']); ?>"><?php echo $row['category_name'] ?></a></p>
-                            <p class="desc"><?php echo strip_tags(character_limiter($row['posting_description'], 130)) ?></p>
+                        
+                        <div class="media-body">
+                          <h3><a href="<?php echo posting_url($row) ?>" > <?php echo character_limiter($row['posting_title'], 40) ?></a> <span class="text-muted small"> - UI Engineer</span></h3>
+                          <address>
+                            <h5>
+                              <span class=""><?php echo pretty_date($row['posting_published_date'], 'l, d-M-Y', FALSE) ?> </span> - 
+                              <span class="">Category: <a href="<?php echo site_url('posting/category/' . $row['posting_category_id']) ?>">
+                                <?php echo $row['category_name'] ?></a> </span>
+                              </h5>
+                            </address>
+                            <?php echo strip_tags(character_limiter($row['posting_description'], 500)) ?>
+                          </div>
                         </div>
-                    </li>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+                    
                 <?php endforeach ?>
 
-            </ul>
+
         </div>
 
     </div>
