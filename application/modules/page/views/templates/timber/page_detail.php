7d9a31544b95e34d8a715e2f7b79968c3b4d87ce
Muhammad Nazih <nazih@artikulpi.com>
2016-05-18 13:31:03 +0700

    Design:MOdifikasi tampilan contact groot template timber

--- application/modules/page/views/templates/timber/page_detail.php
+++ application/modules/page/views/templates/timber/page_detail.php
@@ -5,6 +5,11 @@
         <div class="">
             <div class="tag">
                 <h1><?php echo $page['page_name'] ?></h1>
+                <div class="row-fluid">
+                    <h5>
+                        <span class=""><?php echo pretty_date($page['page_published_date'], 'l, d-M-Y'), FALSE ?></span> - 
+                    </h5>
+                </div>
                 <div class="row">
                     <?php if ($page['page_image'] != NULL) { ?>
                         <div class="col-md-12">
@@ -12,11 +17,7 @@
                         </div>
                     <?php } ?>
                 </div>
-                <div class="row-fluid">
-                    <h5>
-                        <span class=""><?php echo pretty_date($page['page_published_date'], 'l, d-M-Y'), FALSE ?></span> - 
-                    </h5>
-                </div>
+                
                 <div class="row">
                     <div class="col-md-12">
                         <div class="up-teks">
