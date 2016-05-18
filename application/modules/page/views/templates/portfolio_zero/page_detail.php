9b5509d823b7c147c4bcd7f4386729e12a2e1b5c
Muhammad Nazih <nazih@artikulpi.com>
2016-05-18 13:29:25 +0700

    Design:MOdifikasi tampilan contact groot template zero

--- application/modules/page/views/templates/portfolio_zero/page_detail.php
+++ application/modules/page/views/templates/portfolio_zero/page_detail.php
@@ -1,22 +1,23 @@
-
+ <br><br><br><br><br class="hidden-xs">
 <div class="container down">
 <div class="col-md-12 col-sm-12 col-xs-12">
     <div class="row">
         <div class="">
             <div class="tag">
                 <h1><?php echo $page['page_name'] ?></h1>
-                <div class="row">
-                    <?php if ($page['page_image'] != NULL) { ?>
-                        <div class="col-md-12">
-                            <img src="<?php echo $page['page_image'] ?>" alt="Placeholder" class="img-responsive imageds">
-                        </div>
-                    <?php } ?>
-                </div>
                 <div class="row-fluid">
                     <h5>
                         <span class=""><?php echo pretty_date($page['page_published_date'], 'l, d-M-Y'), FALSE ?></span> - 
                     </h5>
                 </div>
+                <div class="row">
+                    <?php if ($page['page_image'] != NULL) { ?>
+                        <div class="col-md-12">
+                            <img  alt="Placeholder" class="img-responsive imageds" src="<?php echo $page['page_image'] ?>"><p></p>
+                        </div>
+                    <?php } ?>
+                </div>
+                
                 <div class="row">
                     <div class="col-md-12">
                         <div class="up-teks">
